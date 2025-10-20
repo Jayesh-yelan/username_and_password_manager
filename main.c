c940db218adc81e59c7cb27f488e1c1e92086e93
Jayesh-yelan <230490131039.jayesh@gmail.com>
2025-10-20 13:00:31 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,5 +4,40 @@
 
 int main()
 {
+  int running = 1;
+  int choise;
+  while(running)
+    {
+      printf("============================================\n");
+      printf("Welcome to the username and password manager\n");
+      printf("============================================\n");
+      printf("Press 1 to make a new entry\n");
+      printf("Press 2 to view all entries\n");
+      printf("Press 3 to view entry by site name\n");
+      printf("Press 4 to delete a entry\n);
+      printf("Press 5 to exit \n);
+      printf("============================================\n");
+      scanf("%d",&choise);
+
+      switch(choise)
+      {
+        case 1:
+          break;
+        case 2:
+          break;
+        case 3:
+          break;
+        case 4:
+          break;
+        case 5:
+          running = 0;
+          break;
+        default:
+          running = 0;
+          break;
+      }
+      
+
+    }
   
 }
